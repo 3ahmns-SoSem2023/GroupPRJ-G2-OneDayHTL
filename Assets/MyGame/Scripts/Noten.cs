352034fd4a606ac542edc8f4e7f4597e30587800
DaveHagenauer <david.hagenauer05@gmail.com>
2023-06-14 17:26:10 +0200

    Schummeln

--- Assets/MyGame/Scripts/Noten.cs
+++ Assets/MyGame/Scripts/Noten.cs
@@ -11,13 +11,41 @@ public class Noten : MonoBehaviour
     public CoutScript02 countScript02;
     public Button abgeben;
     public bool abfrageAbgeben;
+    private bool schummeln;
+    public Button schummelButton;
+    public Button nichtSchummelButton;
 
     // Start is called before the first frame update
     void Start()
     {
         countScript02 = GetComponent<CoutScript02>();
     }
-
+    public void SchummelButton()
+    {
+        schummeln = true;
+        schummelButton.gameObject.SetActive(false);
+        nichtSchummelButton.gameObject.SetActive(false);
+        abgeben.gameObject.SetActive(true);
+        int erwischt = Random.Range(0, 100);
+        if (erwischt < 50)
+        {
+          countScript02.lernCount +=3  ;
+            Debug.Log(countScript02.lernCount);
+        }
+        else
+        {
+            countScript02.lernCount -= 10;
+        }
+       
+    }
+    public void NichtSchummelButton()
+    {
+        schummeln = false;
+        schummelButton.gameObject.SetActive(false);
+        nichtSchummelButton.gameObject.SetActive(false);
+        abgeben.gameObject.SetActive(true);
+    }
+    
     public void NoteBerechnen()
     {
         if (countScript02.lernCount <= 2)
@@ -74,5 +102,7 @@ public class Noten : MonoBehaviour
         abgeben.gameObject.SetActive(false);
         glueckwunschText.gameObject.SetActive(true);
         note.gameObject.SetActive(true);
+        
+
     }
 }
